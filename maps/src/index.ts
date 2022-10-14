4e813b3b8d4fad0294b2182234a0fef1620d215a
Azdevpp <Azdevpp@gmail.com>
2022-10-15 01:39:50 +0300

    MapsWorking

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,7 +1,23 @@
-import { User } from "./User";
-import { Company } from "./Company";
-const user = new User();
-console.log(user);
+//Need To be in the first line for it to work
+///<reference types="@types/google.maps"/>
 
-const company = new Company();
-console.log(company);
+// import { User } from "./User";
+
+// import { Company } from "./Company";
+// let map: google.maps.Map;
+// const center: google.maps.LatLngLiteral = { lat: 30, lng: -110 };
+
+
+// function initMap(): void {
+//   new google.maps.Map(document.getElementById('map') as HTMLElement);
+// }
+// initMap();
+const map = new google.maps.Map(document.getElementById('map')as HTMLElement, {
+  center: {
+    lat: 30,
+    lng: 10
+  },
+  zoom: 10,
+});
+
+// console.log(map);
