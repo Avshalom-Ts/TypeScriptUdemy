9988e965bf702dfbc8d340b1eee2a4bbdd4eddad
Azdevpp <Azdevpp@gmail.com>
2022-10-16 23:38:23 +0300

    FixStatCompareWorkingNow

--- statsCompare/build/analyzers/WinsAnalysis.js
+++ statsCompare/build/analyzers/WinsAnalysis.js
@@ -10,15 +10,14 @@ var WinsAnalysis = /** @class */ (function () {
         var wins = 0;
         for (var _i = 0, matches_1 = matches; _i < matches_1.length; _i++) {
             var match = matches_1[_i];
-            if (match[1] === 'Man United' && match[5] === MatchResult_1.MatchResult.HomeWin) {
+            if (match[1] === this.team && match[5] === MatchResult_1.MatchResult.HomeWin) {
                 wins++;
             }
-            else if (match[2] === 'Man United' &&
-                match[5] === MatchResult_1.MatchResult.AwayWin) {
+            else if (match[2] === this.team && match[5] === MatchResult_1.MatchResult.AwayWin) {
                 wins++;
             }
         }
-        return "Team " + this.team + " won " + wins + " games";
+        return "Team ".concat(this.team, " won ").concat(wins, " games");
     };
     return WinsAnalysis;
 }());
--- statsCompare/src/index.ts
+++ statsCompare/src/index.ts
@@ -2,7 +2,8 @@ import { MatchReader } from './MatchReader';
 import { Summary } from './Summary';
 
 const matchReader = MatchReader.fromCsv('football.csv');
-const summary = Summary.winsAnalysisWithHtmlReport('Man United');
+const summary = Summary.winsAnalysisWithHtmlReport('Tottenham');
 
 matchReader.load();
+// summary.buildAndPrintReport(matchReader.matches);
 summary.buildAndPrintReport(matchReader.matches);
